76513a8bbc29562e5c734e30d290b74c61dd6da4
Ksenx03 <106536997+Ksenx03@users.noreply.github.com>
2022-06-01 17:15:59 +0200

    Add files via upload

---
+++ TestBot2/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new simpleBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
